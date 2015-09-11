eeec7781ef85c6a912a79efcd47fc0898fdbed85
Ryan Norton <rnorton@instructure.com>
2015-09-11 11:16:13 -0600

    File and method refactors for all facilities, error handling modifications to all facilities, timeout changes, change to dryscrape from pyqt4, changes to heroku requirements, changes to heroku procfile

--- facilities.py
+++ facilities.py
@@ -1,18 +1,6 @@
 from utah_soccer import utah_soccer_run
 from lets_play import lets_play_run
-from distutils.core import setup, Command
 import time
-
-setup(name='dryscrape',
-      version='1.0',
-      description='a lightweight Javascript-aware, headless web scraping library for Python',
-      author='Niklas Baumstark',
-      author_email='niklas.baumstark@gmail.com',
-      license='MIT',
-      url='https://github.com/niklasb/dryscrape',
-      packages=['dryscrape', 'dryscrape.driver'],
-      install_requires=['webkit_server>=1.0', 'lxml', 'xvfbwrapper'],
-      )
 
 hours = 12
 
@@ -23,10 +11,10 @@ while True:
   print "Finished Let's Play run at: %s" % time.ctime()
   print "==========================================================================================="
 
-  print "==========================================================================================="
-  print "Running UtahSoccer at: %s" % time.ctime()
-  utah_soccer_run()
-  print "Finished UtahSoccer run at: %s" % time.ctime()
-  print "==========================================================================================="
+#  print "==========================================================================================="
+#  print "Running UtahSoccer at: %s" % time.ctime()
+#  utah_soccer_run()
+#  print "Finished UtahSoccer run at: %s" % time.ctime()
+#  print "==========================================================================================="
 
   time.sleep(hours*60*60)
