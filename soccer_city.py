f58ed539dcd6390870827abbd6d85bfa9983604e
Ryan Norton <rnorton@instructure.com>
2015-10-20 17:52:29 -0600

    fixed timeout

--- soccer_city.py
+++ soccer_city.py
@@ -3,6 +3,113 @@ import requests,json,httplib,sys,dryscrape,urllib,time, datetime, threading
 
 applicationId = "UnWG5wrHS2fIl7xpzxHqStks4ei4sc6p0plxUOGv"
 apiKey = "g7Cj2NeORxfnKRXCHVv3ZcxxjRNpPU1RVuUxX19b"
+
+#
+# To be run before 'fullGameListUpdate()' to seed iterable data.
+# Scrapes the Soccer City site to obtain a list of all youth teams playing currently at Soccer City.
+#
+def youthTeamListUpdate():
+	count = 0
+	connection = httplib.HTTPSConnection('api.parse.com', 443, timeout=120)
+	connection.connect()
+	divisions = []
+	while len(divisions) == 0:
+		url= 'http://www.soccercityutah.com/#!youthschedules/c14mx'
+		session = dryscrape.Session(base_url = url)
+		session.set_timeout(30)
+		session.visit(url)
+		soup = BeautifulSoup(session.body())
+		divisions = soup.findAll("a", {"target": "_blank"})
+	del divisions[15]
+	del divisions[-1]
+	del divisions[-1]
+	for division in divisions:
+	  retries = 0
+          while True:
+            try:
+		url= division['href']
+		print "URL " + url
+		stringDivision = ""
+		if division.span is None:
+			stringDivision = division.contents[0]
+		else:
+			stringDivision = division.span.contents[0]
+
+		print stringDivision + " %s" % count
+		print "\n\n"
+		session = dryscrape.Session(base_url = url)
+		session.set_timeout(30)
+		session.visit(url)
+		soup = BeautifulSoup(session.body())
+		teams = soup.findAll("a")
+		count += 1
+		if soup.find("a", {"href": "#stats"}) is not None:
+			del teams[0]
+
+
+		for x in xrange(1,18):
+			del teams[0]
+
+		betterstrat = ""
+		stringDivision = stringDivision.replace(u'\xa0', ' ')
+		betterstrat = soup.findAll("table",{"id": "ctl00_C_Standings_GridView1"})
+
+		teams2 = betterstrat[0].findAll("tr")
+		del teams2[0]
+
+		for thing in teams2:
+				teamName = thing.findChildren()[0].findChildren()[0].contents[0]
+				teamURL = thing.findChildren()[0].findChildren()[0]['href']
+				splitUrl = teamURL.split('/')
+
+				if stringDivision.split(' ')[1] == "30":
+					teamId = splitUrl[1]
+				else:
+					teamId = splitUrl[4]
+
+				params = urllib.urlencode({"where":json.dumps({
+				"teamId": teamId})})
+				connection.request('GET', '/1/classes/SoccerCityTeams?%s' % params,'', {
+				     "X-Parse-Application-Id": applicationId,
+				     "X-Parse-REST-API-Key": apiKey,
+				   })
+				results = json.loads(connection.getresponse().read())
+				# Object doesn't exist, POST to create new.	
+
+				if results.values() == [[]]:
+					call = 'POST'
+					objId = ''
+				# Object exists, PUT to update existing.
+				else: 
+					call = 'PUT'
+					# Better way to obtain objectID for update?  (nested dictionary/array/dictionary is ugly!  Stupid Python...)
+					objId = '/%s' % results['results'][0]['objectId']
+
+				connection.request(call, '/1/classes/SoccerCityTeams%s' % objId, json.dumps({
+				           "teamId": teamId,
+				           "name": teamName,
+				           "division": stringDivision
+				         }), {
+				           "X-Parse-Application-Id": applicationId,
+				           "X-Parse-REST-API-Key": apiKey,
+				           "Content-Type": "application/json"
+				         })
+				results = json.loads(connection.getresponse().read())
+
+				print results
+	    except Exception, e:
+              print str(e)
+              retries += 1
+              if retries < 5:
+                print "Error retry %s..." % retries
+                time.sleep(5)
+                connection = httplib.HTTPSConnection('api.parse.com', 443, timeout=120)
+                connection.connect()
+                continue
+              else:
+                print "There was a failure in teamListUpdate() for SoccerCity, could not resolve after 5 attempts, aborting..."
+                break
+            break
 #
 # To be run before 'fullGameListUpdate()' to seed iterable data.
 # Scrapes the Soccer City site to obtain a list of all teams in (currently) facility 12 and stores the team data in the 'Teams' table of the Parse DB.
@@ -161,7 +268,7 @@ def gamesUpdate(teamId, teamName, session):
 				location = gameData[12].findChildren()[0].contents[0]
 
 				if game_time.strip() == "Complete":
-					game_time = getGameTime(gameData[10].findChildren()[0]['href'])
+					game_time = getGameTime(gameData[10].findChildren()[0]['href'], session)
 
 				dateSplit = date.split("-")
 				dateSplit2 = dateSplit[1].split(" ")
@@ -325,13 +432,13 @@ def fullGameListUpdate():
                 break
             break
 
-def getGameTime(url):
+def getGameTime(url, session):
 	retries = 0
 	while True:
 		try:
-			session = dryscrape.Session()
-        		session.set_attribute('auto_load_images', False)
-        		session.set_timeout(20)
+			#session = dryscrape.Session()
+        		#session.set_attribute('auto_load_images', False)
+        		#session.set_timeout(20)
 			print "second url = " + url
                         session.visit(url)
                         soup2 = BeautifulSoup(session.body())
@@ -348,5 +455,5 @@ def getGameTime(url):
 			break
 
 
-teamListUpdate()
-fullGameListUpdate()
+# teamListUpdate()
+# fullGameListUpdate()
